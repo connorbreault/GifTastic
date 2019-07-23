ff09163d0aee3632cf67259380f240de4c85b48d
Spaghetti Jones <connor.breault@comcast.net>
2019-07-22 22:25:27 -0700

    giphy linked

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -4,3 +4,25 @@
 //search button function for ajax query
 //ajax queries the user input in search box
 //GIF START & STOP
+
+
+src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"
+
+$("#gifButt").on("click", function(event) {
+    
+    var userSearch = $("#searchInput").val()
+
+    console.log(userSearch)
+    
+    event.preventDefault();
+
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + userSearch + "&api_key=NcAZq5HVV69EeVh7Dh267J8RixNGJ3zn&limit=10"
+    
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+        console.log(response)
+      $("#gifContainer").text();
+    })
+})
