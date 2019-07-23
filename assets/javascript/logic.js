2c408327c28e893c9a4a630aaeb116dfc06d71de
Spaghetti Jones <connor.breault@comcast.net>
2019-07-22 18:03:46 -0700

    html objects added

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,1 +1,6 @@
-//pseudocode here
+//GIPHY BASICS
+//add html search box and button
+//set up gif ajax
+//search button function for ajax query
+//ajax queries the user input in search box
+//GIF START & STOP
